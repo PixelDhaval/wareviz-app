626f64b7b16a9a735d3153a8aebf0ae6182e03b5
admin <admin@gmail.com>
2025-03-19 12:29:10 +0530

    initial commit

---
+++ src/components/vehicleMovementReport/MovementReportTable.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MovementReportTable = () => {
+    return (
+        <>
+            <div className="card-body">
+                table
+            </div>
+        </>
+    )
+}
+
+export default MovementReportTable;
---
+++ src/components/vehicleMovementReport/MovementReportTableHeader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MovementReportTableHeader = () => {
+    return (
+        <>
+            <div className="card-header">
+                <h4 className="card-title">Vehicle Movement Report</h4>
+            </div>
+        </>
+    )
+}
+
+export default MovementReportTableHeader;
